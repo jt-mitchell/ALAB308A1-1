6e15ffe9c589d58de03bd2ed7857581f62768d16
Jeff Mitchell <jeffreytm@gmail.com>
2024-10-27 15:50:24 -0400

    Part 1 main.js updated

--- main.js
+++ main.js
@@ -1,86 +1,16 @@
-// Javascript Data Types
+// Part 1
+let counter = 0;
+function addCount() {
+  try {
+    counter++;
 
-// Number / integers
-// String
-// Boolean
-// Null
-// Undefined
+    console.log("Counter value is: ", counter);
+    addCount(counter);
+  } catch (error) {
+    console.error("Error during adding the count:", error);
+    console.error("Final counter value:", counter);
+  }
+}
+addCount(); // Final counter value: 6802
 
-//Symbol
-// Object
-
-
-
-// Number Data Type
-
-// Integers
-//  -1, 0, 1, 2,
-
-// Floats
-// 2.718 , 3.14, .5, .25 
-
-
-// Arithmetic Operators
-
-// 1 + 2
-// => 3
-
-// 1 - 2
-// => -1
-
-//  10 / 2
-// => 5
-
-// 6 * 2
-// => 12
-
-
-// Math.pow(3, 2)
-// => 9
-
-// console.log(Math.pow(3,2))
-
-// console.log(Math.sqrt(4));
-
-// returns a random float
-// console.log(Math.random())
-
-//round down to the nearest integer
-// console.log(Math.floor(3.14))
-
-//round up to the nearest integer
-// console.log(Math.ceil(3.9999))
-
-// console.log(Math.round(3.14))
-// console.log(Math.round(3.9))
-
-
-// String Data Type
-
-// 1. Surround a word or phrase with double quotes
-// "like this"
-
-// 2. single quotes
-// 'like this'
-
-// 3. backticks
-  // `like this`
-
-// String helper methods
-  
-// console.log('Da Bronx'.length);
-// => 8 
-
-// console.log('hello'.charAt(4));
-
-// console.log('hello'.toUpperCase())
-
-// String Concatenation
-// console.log('hello, ' + 'world')
-
-// console.log(2 + 3 + '50')
-
-// console.log(typeof parseInt('123'));
-
-// console.log(typeof parseFloat('1.2737373'));
-
+// Part 2
